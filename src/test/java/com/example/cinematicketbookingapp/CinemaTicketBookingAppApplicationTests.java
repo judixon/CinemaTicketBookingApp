6d1a28e030402ac47bc2d9c6a004b6a117b380b7
tomekw <tomaszwazny97@gmail.com>
2022-12-14 13:22:03 +0100

    Initial commit

---
+++ src/test/java/com/example/cinematicketbookingapp/CinemaTicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinematicketbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
