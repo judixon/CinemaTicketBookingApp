df068a402a7727f7a2d849682b3e00c723dab1b5
tomekw <tomaszwazny97@gmail.com>
2022-12-16 18:59:27 +0100

    API Exception Handler small changes

--- src/main/java/com/example/cinematicketbookingapp/exceptions/globalexceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/example/cinematicketbookingapp/exceptions/globalexceptionhandler/ApiExceptionHandler.java
@@ -3,10 +3,15 @@ package com.example.cinematicketbookingapp.exceptions.globalexceptionhandler;
 import com.example.cinematicketbookingapp.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -20,6 +25,19 @@ public class ApiExceptionHandler {
                 ZonedDateTime.now()
         );
         return new ResponseEntity<>(apiException, apiException.responseHttpStatus());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
 }
