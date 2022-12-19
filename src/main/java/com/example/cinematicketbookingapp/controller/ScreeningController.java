4f4734bb88648f38f72bd3689e9be2a07898044e
tomekw <tomaszwazny97@gmail.com>
2022-12-19 20:02:24 +0100

    code refactor

--- src/main/java/com/example/cinematicketbookingapp/controller/ScreeningController.java
+++ src/main/java/com/example/cinematicketbookingapp/controller/ScreeningController.java
@@ -3,6 +3,7 @@ package com.example.cinematicketbookingapp.controller;
 import com.example.cinematicketbookingapp.dto.ScreeningDetailsDto;
 import com.example.cinematicketbookingapp.dto.ScreeningListDto;
 import com.example.cinematicketbookingapp.service.ScreeningService;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -20,8 +21,8 @@ public class ScreeningController {
     private final ScreeningService screeningService;
 
     @GetMapping
-    ResponseEntity<List<ScreeningListDto>> getScreenings(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fromDateTime,
-                                                         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime toDateTime,
+    ResponseEntity<List<ScreeningListDto>> getScreenings(@RequestParam @ApiParam(example = "2023-01-01T00:00:00") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fromDateTime,
+                                                         @RequestParam @ApiParam(example = "2023-10-01T00:00:00") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime toDateTime,
                                                          @RequestParam(defaultValue = "startDateTime") String firstSortParam,
                                                          @RequestParam(defaultValue = "movie.title") String secondSortParam,
                                                          @RequestParam(defaultValue = "ASC") Sort.Direction firstParamSortDirection,
